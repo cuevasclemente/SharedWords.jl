4645c7e39f0207ecc7318fffeb5a8ace380743e3
cuevasclemente <cuevasclemente@gmail.com>
2014-09-08 15:10:16 -0400

    Added levthresh to orderedsharedwords

--- src/SharedWords.jl
+++ src/SharedWords.jl
@@ -29,7 +29,7 @@ end
 	
 
 
-function orderedsharedwords(larger,smaller)
+function orderedsharedwords(larger,smaller,levthresh)
 	"Returns the number of shared words between two strings, where the first argument is shorter than the second"
 	smallwords = get_words(smaller)
 	words_shared = is_words_in_string(smallwords,larger)
